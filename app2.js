37a82ee1124b29974050091073cc99118177bbb7
boatio <37399578+boatio@users.noreply.github.com>
2018-04-20 21:43:40 +0900

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();var type = [];
+var http = require('http').Server(app);
+var io = require('socket.io')(http);var prot = 80;var ip;
+app.use(express.static('public')); var playing = 0;var loging = null; var id = null;
+http.listen(prot,'58.236.223.208',function () {
+  console.log('sever is on by localhost:3000');
+  http.close(function(){http.listen(prot,'58.236.223.208');
+});});
+io.on('connection',function(socket) {
+ip =  socket.request.connection.remoteAddress;
+id = socket.id;
+type[playing] = id;
+io.emit('id',type,id,playing);
+playing = playing + 1;
+console.log(':coming ip:' + ip + id);
+socket.on('xy',function(x,y){
+        id = socket.id;
+        //if(data.direc != null)
+        //io.emit('xyr',data.x,data.y,data.direc,id);
+        io.emit('xyr',x,y,id);
+    });
+});
